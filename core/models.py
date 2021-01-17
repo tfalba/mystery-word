bba8872f18eadb82dc9c6887d24d463a62d3ab32
Tracy Falba <tfalba@mac.com>
2021-01-16 21:22:15 -0500

    fixing path

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 import random
 import requests
 
-ALL_WORDS = open('../words.txt').read().lower()
+ALL_WORDS = open('/core/words.txt').read().lower()
 # ALL_WORDS = open('/Users/tracyfalba/momentum/django-projects/locallibrary/catalog/words.txt', 'r').read().lower()
 ALL_WORDS = ALL_WORDS.split()
 ALL_SMALL_WORDS = []
