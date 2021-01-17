58c33bb34edb9965d31bac8920a8764189460bfd
Tracy Falba <tfalba@mac.com>
2021-01-16 22:03:27 -0500

    updated debug

--- mystery_word/settings.py
+++ mystery_word/settings.py
@@ -33,7 +33,8 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
